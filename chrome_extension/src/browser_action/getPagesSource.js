6fbfacf73311f6570e1f837241b221d1a901ace6
Jen Person <personjeh@gmail.com>
2018-11-07 14:47:41 -0800

    found another componened of the scraper-- the need to query the selector of quesitons people are writing. haven't implemented it yet, but wrote it down

--- chrome_extension/src/browser_action/getPagesSource.js
+++ chrome_extension/src/browser_action/getPagesSource.js
@@ -1,6 +1,9 @@
 function DOMtoString(document_root) {
   var html = '';
   // pull content of question
+  // question in progress
+  //var question = document_root.querySelector(".wmd-preview").innerHTML;
+  // published question
   var question = document_root.querySelector(".post-text").innerHTML;
   html += question;
   return html;
