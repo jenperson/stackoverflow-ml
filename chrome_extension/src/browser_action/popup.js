69c18be4cc8ea8a94436b2134befb66ab08b8cf9
Sara Robinson <thesararobinson@gmail.com>
2018-11-07 13:12:05 -0500

    Add logging to extension

--- chrome_extension/src/browser_action/popup.js
+++ chrome_extension/src/browser_action/popup.js
@@ -1,4 +1,4 @@
-var bkg = chrome.extension.getBackgroundPage();
+const bkg = chrome.extension.getBackgroundPage();
 
 let parseClassification = (response) => {
     let classifications = response[0].payload;
@@ -6,6 +6,7 @@ let parseClassification = (response) => {
 
     for (let i in classifications) {
         let data = classifications[i];
+        bkg.console.log(data); // Logs to background.html
         let label = data.displayName;
         let score_pct = data.classification.score * 100;
         displayStr += `${label}: ${score_pct.toFixed(1)}%\n`;
@@ -15,6 +16,7 @@ let parseClassification = (response) => {
 
 $('#call-model').on('click', (e) => {
     let text = $('#question').val();
+    bkg.console.log(`Sending question ${text} to model...`);
     let url = `https://us-central1-automl-and-firebase.cloudfunctions.net/stackoverflow-manual?text=${text}`;
     $('#status').text('Sending to model...');
     $.post(url, (data) => {
