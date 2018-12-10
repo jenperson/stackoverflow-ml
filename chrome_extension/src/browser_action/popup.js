687156bae8de978e40b3726948929cef5278524b
Jen Person <personjeh@gmail.com>
2018-12-10 15:27:00 -0800

    minor visual cleanup of code

--- chrome_extension/src/browser_action/popup.js
+++ chrome_extension/src/browser_action/popup.js
@@ -1,6 +1,4 @@
 const bkg = chrome.extension.getBackgroundPage();
-let label;
-
 
 let addTabs = (response) => {
   let classifications = response[0].payload;
@@ -8,22 +6,21 @@ let addTabs = (response) => {
   for (let label of top5) {
     var selector = {label: label};
       
-      chrome.tabs.executeScript({
-        code: '(' + function(params) {
-          document.querySelector('.tag-editor.s-input').insertAdjacentHTML('afterbegin',
-          '<span><span class="s-tag rendered-element">'+params.label+'<a class="js-delete-tag s-tag--dismiss" title="Remove tag"><svg style="pointer-events:none;" class="svg-icon iconClearSm" width="12" height="12" viewBox="0 0 14 14"><path d="M12 3.41L10.59 2 7 5.59 3.41 2 2 3.41 5.59 7 2 10.59 3.41 12 7 8.41 10.59 12 12 10.59 8.41 7z"></path></svg></a></span></span>'
-          );
-            return {success: true, html: document.body.innerHTML};
-        } + ')(' + JSON.stringify(selector) + ');'
-      });
-      chrome.tabs.executeScript({
-        code: '(' + function(params) {
-          document.querySelector('#tagnames').value += params.label + " "
-          console.log("like yeah", params.label);
-            return {success: true, html: document.body.innerHTML};
-        } + ')(' + JSON.stringify(selector) + ');'
-      });
-    }
+    chrome.tabs.executeScript({
+      code: '(' + function(params) {
+        document.querySelector('.tag-editor.s-input').insertAdjacentHTML('afterbegin',
+        '<span><span class="s-tag rendered-element">'+params.label+'<a class="js-delete-tag s-tag--dismiss" title="Remove tag"><svg style="pointer-events:none;" class="svg-icon iconClearSm" width="12" height="12" viewBox="0 0 14 14"><path d="M12 3.41L10.59 2 7 5.59 3.41 2 2 3.41 5.59 7 2 10.59 3.41 12 7 8.41 10.59 12 12 10.59 8.41 7z"></path></svg></a></span></span>'
+        );
+          return {success: true, html: document.body.innerHTML};
+      } + ')(' + JSON.stringify(selector) + ');'
+    });
+    chrome.tabs.executeScript({
+      code: '(' + function(params) {
+        document.querySelector('#tagnames').value += params.label + " "
+          return {success: true, html: document.body.innerHTML};
+      } + ')(' + JSON.stringify(selector) + ');'
+    });
+  }
 }
 
 let getTop5 = (response) => {
