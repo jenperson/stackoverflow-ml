d5c30e8d5dc03026cfd785fab3b1e76702e9e56d
Jen Person <personjeh@gmail.com>
2018-12-10 15:20:14 -0800

    resolved issue where tags would appear on screen, but error would happen when trying to submit question. Now works for most tags BUT we need to handle tags that have dashes in them. The AutoML model currently returns them as underscores (like in objective_c) which is not a tag in the eys of StackOverflow.

--- chrome_extension/src/browser_action/popup.js
+++ chrome_extension/src/browser_action/popup.js
@@ -4,29 +4,47 @@ let label;
 
 let addTabs = (response) => {
   let classifications = response[0].payload;
-  console.log("addtabs")
-  //console.log(classifications);
-  for (let i in classifications) {
-      let data = classifications[i];
-      console.log(data);
-      label = data.displayName;
-      console.log(label);
+  let top5 = getTop5(classifications)
+  for (let label of top5) {
+    var selector = {label: label};
+      
+      chrome.tabs.executeScript({
+        code: '(' + function(params) {
+          document.querySelector('.tag-editor.s-input').insertAdjacentHTML('afterbegin',
+          '<span><span class="s-tag rendered-element">'+params.label+'<a class="js-delete-tag s-tag--dismiss" title="Remove tag"><svg style="pointer-events:none;" class="svg-icon iconClearSm" width="12" height="12" viewBox="0 0 14 14"><path d="M12 3.41L10.59 2 7 5.59 3.41 2 2 3.41 5.59 7 2 10.59 3.41 12 7 8.41 10.59 12 12 10.59 8.41 7z"></path></svg></a></span></span>'
+          );
+            return {success: true, html: document.body.innerHTML};
+        } + ')(' + JSON.stringify(selector) + ');'
+      });
+      chrome.tabs.executeScript({
+        code: '(' + function(params) {
+          document.querySelector('#tagnames').value += params.label + " "
+          console.log("like yeah", params.label);
+            return {success: true, html: document.body.innerHTML};
+        } + ')(' + JSON.stringify(selector) + ');'
+      });
+    }
+}
+
+let getTop5 = (response) => {
+  let label_arr = [];
+
+  for (let data of response) {
+      let label = data.displayName;
       let score_pct = data.classification.score * 100;
-      if (score_pct > 1) {
-        var selector = {label: label};
-        
-        chrome.tabs.executeScript({
-          code: '(' + function(params) {
-            document.querySelector('.tag-editor.s-input').insertAdjacentHTML('afterbegin',
-            '<span><span class="s-tag rendered-element">'+params.label+'<a class="js-delete-tag s-tag--dismiss" title="Remove tag"><svg style="pointer-events:none;" class="svg-icon iconClearSm" width="12" height="12" viewBox="0 0 14 14"><path d="M12 3.41L10.59 2 7 5.59 3.41 2 2 3.41 5.59 7 2 10.59 3.41 12 7 8.41 10.59 12 12 10.59 8.41 7z"></path></svg></a></span></span>'
-            );
-              return {success: true, html: document.body.innerHTML};
-          } + ')(' + JSON.stringify(selector) + ');'
-        });
-      }
+      label_arr.push([label, score_pct]);
   }
-
-
+  // Sort the list of labels by confidence
+  label_arr.sort((a, b) => {
+    return b[1] - a[1];
+  });
+  let top_5 = label_arr.slice(0,5);
+  let tags = [];
+  console.log(response);
+  for (let data of top_5) {
+    tags.push(data[0]);
+  }
+  return tags;
 }
 
 let parseClassification = (response) => {
